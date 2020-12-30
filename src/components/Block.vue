89745c18c59d37ab3498d94a93c2638808ac3c6b
hollywoodpig <nekitkld@gmail.com>
2020-12-30 11:48:02 +0200

    component division

---
+++ src/components/Block.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block"></div>
+</template>
+
+<script>
+export default {
+  name: "Block"
+};
+</script>
+
+<style lang="scss" scoped>
+.block {
+  background: #9a142d;
+  width: 220px;
+  height: 220px;
+  border-radius: 0.3em;
+  box-shadow: 7px 7px 4px transparentize(#000, 0.74);
+  margin: 0 auto 75px;
+}
+</style>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -2,34 +2,25 @@
   <div class="result">
     <ContentWrapper>
       <Subtitle>результат:</Subtitle>
-      <div class="block"></div>
+      <Block />
       <Input />
     </ContentWrapper>
   </div>
 </template>
 
 <script>
-import Input from "./ui/Input";
+import Input from "./Input";
 import Subtitle from "./ui/Subtitle";
 import ContentWrapper from "./ui/ContentWrapper";
+import Block from "./Block";
 
 export default {
   name: "Result",
   components: {
+    Block,
     Input,
     Subtitle,
     ContentWrapper
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.block {
-  background: #9a142d;
-  width: 220px;
-  height: 220px;
-  border-radius: 0.3em;
-  box-shadow: 7px 7px 4px transparentize(#000, 0.74);
-  margin: 0 auto 75px;
-}
-</style>
